7c29d5d74930da60888da3c3204213956de4060c
aishurj669 <77617525+aishurj669@users.noreply.github.com>
2021-01-25 17:26:30 +0530

    Add files via upload

--- Doctorpage.dart
+++ Doctorpage.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter22_app/FindDoctorpage.dart';
+import 'package:flutter22_app/Loginpage.dart';
 
 class Doctorpage extends StatefulWidget{
   @override
@@ -19,84 +22,37 @@ class Doctor_Session extends State<Doctorpage>{
        title: Text(""),
      ),
      body:
-     Container(
-         decoration: BoxDecoration(
-           gradient: LinearGradient(
-             begin: Alignment.topRight,
-             end: Alignment.bottomLeft,
-           colors: [Colors.deepPurpleAccent,Colors.purpleAccent],),
+     Column(
+         children:[
+           Container(
+             padding: const EdgeInsets.all(30),
+             child: Text("Choose The Doctor You Want",style:TextStyle(fontWeight: FontWeight.bold,fontSize: 40),),
+           ),
+           Container(
+             padding: const EdgeInsets.all(20),
+             child: Text("Lorem ipsum dolor amet, consectetur adipiscing inet deli",style: TextStyle(fontSize: 20),),
+           ),
 
-// //colors
-//          //Color Orange = const Color(0xFFFF881B);
-//       // Color Pink = const Color(0xFFE62E6F);
-//
-    ),
-
-
-
-       child: Column(
-         children: <Widget>[
-            Container(
-              child: Stack(children: <Widget>[ //stack overlaps widgets
-                Opacity( //semi red clippath with more height and with 0.5 opacity
-                 opacity: 0.5,
-                  child: ClipPath(
-                    clipper:WaveClipper(), //set our custom wave clipper
-                    child:Container(
-                    color:Colors.white,
-                    height:200,
-                  ),
-                ),
-              ),
-            ],
-
-              ),
-            ),
-           Text("Choose The Doctor You Want",style:TextStyle(fontWeight: FontWeight.bold,fontSize: 50),),
-           Text("Lorem ipsum dolor amet, consectetur adipiscing inet deli",style: TextStyle(fontSize: 20),),
-           FlatButton(
+           RaisedButton(
             // padding: const EdgeInsets.all(20),
              child: Text("Get Started"),
-             color: Colors.white,
+             color: Colors.redAccent,
              onPressed: (){
-
+               Navigator.push(
+                 context,
+                 MaterialPageRoute(builder: (context) => FindDoctorpage()),
+               );
              },
            ),
-         ],
+           Container(
+            // padding: const EdgeInsets.all(20),
+             child: Image.asset("assets/Images/Doct5.png"),
+            height: MediaQuery.of(context).size.height*0.6,
+            width: MediaQuery.of(context).size.width ,
+           )
+         ]
        ),
-
-     ),
    );
   }
 
-}
-class WaveClipper extends CustomClipper<Path>{
-  @override
-  Path getClip(Size size) {
-
-    var path = new Path();
-    path.lineTo(0, size.height); //start path with this if you are making at bottom
-
-    var firstStart = Offset(size.width / 5, size.height);
-    //fist point of quadratic bezier curve
-    var firstEnd = Offset(size.width / 2.25, size.height - 50.0);
-    //second point of quadratic bezier curve
-    path.quadraticBezierTo(firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy);
-
-    var secondStart = Offset(size.width - (size.width / 3.24), size.height - 105);
-    //third point of quadratic bezier curve
-    var secondEnd = Offset(size.width, size.height - 10);
-    //fourth point of quadratic bezier curve
-    path.quadraticBezierTo(secondStart.dx, secondStart.dy, secondEnd.dx, secondEnd.dy);
-
-    path.lineTo(size.width, 0); //end with this path if you are making wave at bottom
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false; //if new instance have different instance than old instance
-    //then you must return true;
-  }
 }
